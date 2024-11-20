0236d36405fc2e525ea108101bba960f299b35f7
enyllaoliveira <enyllaoliveira@gmail.com>
2024-11-20 17:55:43 -0300

    layout improvement

--- src/layout.tsx
+++ src/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className="overflow-x-hidden scroll-smooth mr-auto my-12 sm:my-4">
         {" "}
         <a
-          className="bg-slate-400 border text-black tems-center justify-center flex flex-row gap-2 w-fit text-sm font-semibold outline-none py-2.5 px-4 rounded-lg transition duration-300 ml-auto hover:bg-slate-300"
+          className="bg-slate-400 border text-black items-center justify-center flex flex-row gap-2 w-fit text-sm font-semibold outline-none py-2.5 px-4 rounded-lg transition duration-300 ml-auto hover:bg-slate-300 mb-4 sm:mb-8"
           href="/registro"
         >
           Entrar no sistema
